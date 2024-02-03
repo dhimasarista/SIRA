0062f16f561ca61e0d51d8e3880563aa3cff443a
dhimasarista <dhimasharrisst@gmail.com>
2024-02-03 19:38:16 +0700

    Attach Middlewares

--- app.js
+++ app.js
@@ -2,24 +2,23 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const setupRoutes = require("./app/routes/routes");
 
-// Init Aplikasi
-const app = express();
-const port = 9999;
-// Init Server
-const server = http.createServer(app);
+const app = express(); // Init Aplikasi
+const port = 9999; // Init Port
+const server = http.createServer(app); // Init Server
 
-// Middlewares
+// Middlewares & Routes
 app.set('view engine', 'ejs'); // Mengatur View Engine ke EJS
 app.set('views', path.join(__dirname, 'views')); // Mengatur path ke folder views
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files dari folder "public" 
 app.use(express.urlencoded({extended: false})); 
 app.use(express.json()); // Parsing Permintaan JSON
 // app.use(cookieParser()); // Menggunakan cookie-parser
-
-// Routes
-const setupRoutes = require("./app/routes/routes");
-setupRoutes(app);
+setupRoutes(app); // Mengatur Routing
+app.use((req, res, next) => {
+    res.status(404).redirect('/404'); // Ganti '/halaman-tertentu' dengan path yang diinginkan
+});
 
 // Running Aplikasi
 server.listen(port, function() {
